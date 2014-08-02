2b182c59286e20d853774c457fcc6a63fdb3f19d
Arata <tarata43@yahoo.co.jp>
2014-08-02 19:21:53 +0900

    404と500の設定

--- public_html/index.php
+++ public_html/index.php
@@ -2,23 +2,35 @@
 
 require '../vendor/autoload.php';
 
-// 初期化
+// 初期化・設定
 
 $app = new \Slim\Slim();
 
+$app->config(array(
+    'templates.path' => '../templates',
+    'debug' => false,
+));
+
 // エラーハンドリング
 
+$app->notFound(function () use ($app) {
+    $app->render('404.html');
+});
 
+$app->error(function (\Exception $e) use ($app) {
+    $app->render('500.html');
+});
 
 // ルーティング
 
-$app->get('/', function ($name) {
-            echo "Hello world";
+$app->get('/', function () {
+    echo "Hello world";
 });
 
-$app->get('/hello/:name', function ($name) {
-            echo "Hello, $name";
+$app->get('/error', function() {
+    throw new Exception();
 });
 
+// 実行
 
 $app->run();
